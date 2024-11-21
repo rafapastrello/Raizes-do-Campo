0f5d09bb963a266f5feb520f846c7b895b4b0353
Pastrello <rrafa.pastrello@gmail.com>
2024-11-21 16:39:23 -0400

    commit

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// Selecione todos os links
+const links = document.querySelectorAll('.nossa-empresa, .trabalho-turma');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        // Remove a classe 'active' de todos os links
+        links.forEach(l => l.classList.remove('active'));
+        // Adiciona a classe 'active' ao link clicado
+        link.classList.add('active');
+    });
+});
